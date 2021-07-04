7977522b92727390e1be872be33f9efcaecf877c
ravi1013 <62096523+ravi1013@users.noreply.github.com>
2021-07-04 13:07:48 +0530

    Update 1_Speaker_Audio_Part_.ino

--- 1_Speaker_Audio_Part_.ino
+++ 1_Speaker_Audio_Part_.ino
@@ -32,36 +32,4 @@ void setup() {
 
 void loop() 
 {
-digitalWrite(trigPin,HIGH);
-delayMicroseconds(500);
-digitalWrite(trigPin,LOW);
-
-duration = pulseIn(echoPin,HIGH); /*it starts counting number of microseconds until echoPin goes High &
-                                  store that value to the variable duration,
-                                   now this duration is then converted to the distance by the formula*/
-distance = (duration/2)/29.1;
-delay(200);
-if(distance<25)
-{
-  tone(buzzled,980);
-  count++;
-
-  Serial.print("Distance = ");
-Serial.print(distance);
-Serial.print("cm");
-Serial.println(" ");
-Serial.print("Number of person = ");
-Serial.print(count);
-Serial.println(" ");
-delay(500);
-Serial.println(" ");
-  {
- (!SD.begin(SD_ChipSelectPin));       //see if the card is present and can be initialized
-                                  
-  tmrpcm.setVolume(6);                    //0 to 7. Set volume level
-  tmrpcm.play("5b.wav");         //the sound file "1" will play each time the arduino powers up, or is reset
-  }
-}
-else
-noTone(buzzled);
 }
